6404a2d22078526171a0655454fd197936ee2de4
rxuby <jutapon.c@kkumail.com>
2024-04-03 13:24:24 +0700

    [Edit] index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log("Hello World")
+
+function hello() {
+    try {
+        let value = new Promise((resolve, reject) => {
+            rl.question('What - s your name ?', (name) => {
+                if (name === "Cake") {
+                    resolve(name);
+                } else {
+                    reject("Error");
+                }
+                rl.close();
+            });
+        });
+        console.log('My name is ${ value }');
+    } catch (e) {
+        console.log(e);
+    }
+}
